4d5237e52067c86a533320f28cbfc62e13247250
Tpxdkss <tpxdkss@gmail.com>
2020-12-15 19:34:51 +0900

    Update

--- Java/KoreaIT/workplace/day02/src/day02/PrintTest2.java
+++ Java/KoreaIT/workplace/day02/src/day02/PrintTest2.java
@@ -2,12 +2,21 @@ package day02;
 
 public class PrintTest2 {
 	public static void main(String[] args) {
-		System.out.print("안녕하세요");
-		System.out.print("\n");
-		System.out.print("홍길동 입니다.\n");
-		System.out.println("Hello\tWorld");
-		System.out.println("\"Hello world\"");
+//		System.out.print("안녕하세요");
+//		System.out.print("\n");
+//		System.out.print("홍길동 입니다.\n");
+//		System.out.println("Hello\tWorld");
+//		System.out.println("\"Hello world\"");
+//		System.out.println("'Hello world'");
 		
+//		주석
+//		한번에
+//		처리하기: Ctrl+ /
+		
+		System.out.println("안녕하세요");
+		System.out.println("\"코리아 IT 아카데미\" 입니다.");
+		System.out.println("'정다솔'\t: 자바1");
+		System.out.println("\\O/");
 	}
 }
 
