3b8ef47c4a8b515f283b73ba1632ce34c54c33aa
Tpxdkss <tpxdkss@gmail.com>
2020-12-15 19:17:33 +0900

    Lecture 02

--- Java/KoreaIT/workplace/Day01/src/day01/PrintTest.java
+++ Java/KoreaIT/workplace/Day01/src/day01/PrintTest.java
@@ -1,5 +1,13 @@
 package day01;
 
 public class PrintTest {
-
+	// 한 줄 주석
+	/*
+	 * 여러 줄 주석
+	 */
+	//main 메소드:
+	public static void main(String[] args) {
+		System.out.print("안녕하세요");
+		System.out.print("홍길동입니다.");
+	}
 }
---
+++ Java/KoreaIT/workplace/day02/src/day02/PrintTest2.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class PrintTest2 {
+	public static void main(String[] args) {
+		System.out.print("안녕하세요");
+		System.out.print("\n");
+		System.out.print("홍길동 입니다.\n");
+		System.out.println("Hello\tWorld");
+		System.out.println("\"Hello world\"");
+		
+	}
+}
+
+
