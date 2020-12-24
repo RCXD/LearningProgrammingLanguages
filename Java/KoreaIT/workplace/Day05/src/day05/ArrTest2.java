e2fe82c26a0bbb0a8dd0fc2c069c468fdb9754d1
Tpxdkss <tpxdkss@gmail.com>
2020-12-24 22:39:44 +0900

    Update

--- Java/KoreaIT/workplace/Day05/src/day05/ArrTest2.java
+++ Java/KoreaIT/workplace/Day05/src/day05/ArrTest2.java
@@ -5,13 +5,13 @@ public class ArrTest2 {
 		//행: 작은 배열의 개수
 		//열: 작은 배열의 칸수
 		int[][] arData = {{1,2,3}, {4,5,6}};
-		for (int i = 0; i < arData.length; i++) {
-			for (int j = 0; j < arData[0].length; j++) {
+		for (int i = 0; i < arData.length; i++) { //arData.length는 행길이 -> 행이 바깥
+			for (int j = 0; j < arData[0].length; j++) { //arData[0].length는 열길이 -> 열이 내부
 				System.out.println(arData[i][j]);
 			}
 		}
 		for (int i = 0; i < 6; i++) {
-			System.out.println(arData[i/3][i%3]); //되기는 한다
+			System.out.println(arData[i/3][i%3]); //되기는 한다. 나누기와 몫으로... interesting
 		}
 	}
 }
--- Java/KoreaIT/workplace/Day06/src/day06/MethodTest.java
+++ Java/KoreaIT/workplace/Day06/src/day06/MethodTest.java
@@ -1,5 +1,31 @@
 package day06;
 
 public class MethodTest {
-
+	public static void main(String[] args) {
+		//규칙성이 없으면, for문으로 여러 상황을 처리하기 어렵다.
+		
+		MethodTest m = new MethodTest();
+		System.out.println(m.f(3)); //f가 정의된 클래스의 객체를 만들고, 객체의 메소드로 불러와야 사용 가능.
+		
+		System.out.println("구구단");
+		m.print99(3); //함수명에 같다대면 리턴값을 바로 알 수 있다.
+		System.out.println(m.plus(10, 5));
+	}
+	//f(x) = 2x+1
+	//정의한 범위를 잘 확인해서 호출해야 한다.
+	int f(int x) { //main과 f는 다른 구역에 정의되어 있다는점을 주의.
+		return 2*x+1;
+	}
+	//구구단 출력 메소드
+	void print99(int dan) {
+		for (int i = 1; i < 10; i++) {
+			System.out.println(dan+" X "+i+" = "+i*dan);
+		}
+	}
+	//두 정수의 덧셈 메소드
+	int plus(int num1, int num2) {
+		int result = num1+num2;
+		System.out.println("두 정수의 덧셈 메소드입니다."); //출력형태도 바꾸고싶을 때가 있다. 따라서 이럴 때에는 리턴을 주는게 좋다.
+		return result;
+	}
 }
